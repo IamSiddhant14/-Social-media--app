06c5bc20b29085f392518e94c030719500317f58
IamSiddhnat14 <siddhantsharma669@gmail.com>
2021-07-25 15:09:45 +0530

    Half way through setting up an scalable working directory

---
+++ codeial/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller')
+
+console.log('ROUTER LOADED');
+
+router.get('/',homeController.home);
+router.use('/users',require('./users'))
+
+
+
+
+
+module.exports = router;
+ 
